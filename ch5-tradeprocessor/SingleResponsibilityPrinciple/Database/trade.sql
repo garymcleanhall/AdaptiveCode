0eda887a82295ca1c1d8d7475a1771f6384a8af0
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2015-01-17 01:18:59 +0000

    Started adding in the example code for Ch5, which went missing.

---
+++ ch5-tradeprocessor/SingleResponsibilityPrinciple/Database/trade.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[trade]
+(
+	[id] INT NOT NULL PRIMARY KEY,
+	[sourceCurrency] CHAR(3) NOT NULL,
+	[destinationCurrency] CHAR(3) NOT NULL,
+	[lots] FLOAT NOT NULL,
+	[price] MONEY NOT NULL
+)
---
+++ ch5-tradeprocessor/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/TradeRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace SingleResponsibilityPrinciple
+{
+    internal class TradeRecord
+    {
+        internal string DestinationCurrency;
+        internal float Lots;
+        internal decimal Price;
+        internal string SourceCurrency;
+    }
+}
