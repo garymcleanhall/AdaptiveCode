3b95cac2bb909725fdc83f351adf47d181c6e404
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2015-01-18 15:30:35 +0000

    Added code for `Refactor for clarity` section

---
+++ ch5-tradeprocessor/RefactoringForClarity/Database/insert_trade.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[insert_trade]
+	@sourceCurrency CHAR(3),
+	@destinationCurrency CHAR(3),
+	@lots FLOAT,
+	@price MONEY
+AS
+	
+	INSERT INTO trade(sourceCurrency, destinationCurrency, lots, price)
+	VALUES(@sourceCurrency, @destinationCurrency, @lots, @price)
+
+RETURN 0
