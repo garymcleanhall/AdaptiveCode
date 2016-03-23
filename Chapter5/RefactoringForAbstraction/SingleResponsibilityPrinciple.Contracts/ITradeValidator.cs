8f1559ba68adea101f43bc28f6406d0a0170e764
erich brunner <erich.brunner@live.at>
2016-03-23 10:29:25 +0100

    Revert "added ITradeProcessor in .Contracts assembly added Client.ConsoleUI"
    This reverts commit 05716e2e11d9df00c4a995a25153feaefbd1602a.

--- Chapter5/RefactoringForAbstraction/SingleResponsibilityPrinciple.Contracts/ITradeValidator.cs
+++ Chapter5/RefactoringForAbstraction/SingleResponsibilityPrinciple.Contracts/ITradeValidator.cs
@@ -1,15 +1,7 @@
 ﻿namespace SingleResponsibilityPrinciple.Contracts
 {
-    /// <summary>
-    /// Interface for TradeValidator strategy implemenations.
-    /// </summary>
     public interface ITradeValidator
     {
-        /// <summary>
-        /// Validates the specified trade data.
-        /// </summary>
-        /// <param name="tradeData">The trade data.</param>
-        /// <returns></returns>
         bool Validate(string[] tradeData);
     }
 }
--- Chapter5/RefactoringForAbstraction/SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ Chapter5/RefactoringForAbstraction/SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,29 +1,16 @@
 ﻿using System;
+
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
-    /// <summary>
-    ///     Logs messages to the console. A strategy pattern implementation of <see cref="ILogger" />.
-    /// </summary>
-    /// <seealso cref="SingleResponsibilityPrinciple.Contracts.ILogger" />
     public class ConsoleLogger : ILogger
     {
-        /// <summary>
-        ///     Logs warning messages.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="args">The arguments.</param>
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
         }
 
-        /// <summary>
-        ///     Logs information messages.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="args">The arguments.</param>
         public void LogInfo(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
